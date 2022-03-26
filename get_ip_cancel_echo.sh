c9f6402309c584d3e57bf374f5896e7cf92429d9
lex <aibangjuxin@qq.com>
2022-03-26 12:09:00 +0800

    add edit check_domain.sh for add curl

--- get_ip_cancel_echo.sh
+++ get_ip_cancel_echo.sh
@@ -9,13 +9,13 @@ function get_ipaddr {
     local query_type=A
     ip_address=""
     # use host command for DNS lookup operations
-    host -t ${query_type} "${hostname}" ${dnsserver} &>/dev/null
-    if [ "$?" -eq "0" ]; then
-        # get ip address
-        ip_address="$(host -t ${query_type} "${hostname}" | awk '/has.*address/{print $NF}' | sort -rn | awk BEGIN{RS=EOF}'{gsub(/\n/,",");print}')"
-    else
-        echo "############"
-    fi
+    #host -t ${query_type} "${hostname}" ${dnsserver} &>/dev/null
+    #if [ "$?" -eq "0" ]; then
+    # get ip address
+    ip_address="$(host -t ${query_type} "${hostname}" | awk '/has.*address/{print $NF}' | sort -rn | awk BEGIN{RS=EOF}'{gsub(/\n/,",");print}')"
+    #else
+    #    echo "############"
+    #fi
     # display ip
     echo "$ip_address"
 }
@@ -28,11 +28,11 @@ awk 'BEGIN{while (a++<50) s=s "-"; print s,"testing output finished",s}'
 awk 'BEGIN{while (a++<50) s=s "-"; print s,"testing read from file",s}'
 IFS=' '
 while read -r domain_array ip_array;do
-    echo "domain_array is ${domain_array}"
-    get_ipaddr "${domain_array}"
+    #echo "domain_array is ${domain_array}"
+    #get_ipaddr "${domain_array}"
     #echo "${domain_array}" "$(get_ipaddr "${domain_array[$i]}")" >>$collect_ip
     echo "${domain_array}" "$(get_ipaddr "${domain_array}")" >>$collect_ip
-    echo "ip_array is ${ip_array}"
+    #echo "ip_array is ${ip_array}"
 done < $domain_ip
 awk 'BEGIN{while (a++<50) s=s "-"; print s,"get_ipaddr and collect ip finished",s}'
 cat $collect_ip
@@ -43,4 +43,5 @@ wc -l /tmp/a.txt
 125 /tmp/a.txt
 ./get_ip.sh  8.68s user 7.22s system 44% cpu 36.105 total
 ./get_ip_cancel_echo.sh  4.29s user 3.69s system 44% cpu 17.943 total
+./get_ip_cancel_echo.sh  3.04s user 2.80s system 61% cpu 9.529 total
 EOF
